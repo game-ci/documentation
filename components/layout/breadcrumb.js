6ae2111a3928752cbaf03e911873b78b091456cf
Webber <webber@takken.io>
2020-05-30 00:16:48 +0200

    Add breadcrumbs

--- components/layout/breadcrumb.js
+++ components/layout/breadcrumb.js
@@ -1,13 +1,27 @@
 import { Breadcrumb } from 'antd';
+import { useRouter } from 'next/router';
 
 const { Item } = Breadcrumb;
 
-const BreadcrumbWrapper = () => (
-  <Breadcrumb style={{ margin: '16px 0' }}>
-    <Item>Home</Item>
-    <Item>List</Item>
-    <Item>App</Item>
-  </Breadcrumb>
-);
+const BreadcrumbWrapper = () => {
+  const { asPath } = useRouter();
+
+  const segments = asPath.split('/');
+  let url = ''
+  const items = segments.map(segment => {
+    url += `${segment}/`;
+    return (
+      <Item style={{ textTransform: 'capitalize' }}>
+        <a href={url.slice(0, -1)}>{segment.replace('-', ' ')}</a>
+      </Item>
+    )
+  });
+
+  return (
+    <Breadcrumb style={{ margin: '16px 0' }}>
+      {items}
+    </Breadcrumb>
+  );
+};
 
 export default BreadcrumbWrapper;
