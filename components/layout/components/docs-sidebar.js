c7429189ea46a901e8d9787014f3564a0808806e
Webber <webber@takken.io>
2020-05-31 03:29:33 +0200

    Readd icons, update packages --latest

--- components/layout/components/docs-sidebar.js
+++ components/layout/components/docs-sidebar.js
@@ -1,6 +1,12 @@
 import { Menu, Layout } from 'antd';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import {
+  GithubOutlined,
+  CheckOutlined,
+  AppstoreAddOutlined,
+  GitlabOutlined,
+} from '@ant-design/icons';
 
 const { Sider } = Layout;
 const { SubMenu } = Menu;
@@ -13,23 +19,26 @@ const DocsSidebar = () => {
       <Menu
         mode="inline"
         defaultSelectedKeys={[currentKey]}
-        defaultOpenKeys={['main', 'github', 'deployment']}
+        defaultOpenKeys={['main', 'github', 'gitlab', 'deployment']}
         style={{ height: '100%' }}
       >
-        <SubMenu key="main" icon={null} title="Unity CI">
+        <SubMenu key="main" icon={<CheckOutlined />} title="Unity CI">
           <Menu.Item key="/docs"><Link href="/docs"><a>Getting started</a></Link></Menu.Item>
           <Menu.Item key="/example"><Link href="/example"><a>Example</a></Link></Menu.Item>
           <Menu.Item key="3">option3</Menu.Item>
           <Menu.Item key="4">option4</Menu.Item>
         </SubMenu>
-        <SubMenu key="github" icon={null} title="Github">
-          <Menu.Item key="/docs/unity-actions"><Link href="/docs/unity-actions"><a>Unity Actions</a></Link></Menu.Item>
+        <SubMenu key="github" icon={<GithubOutlined />} title="GitHub">
+          <Menu.Item key="/docs/unity-actions"><Link href="/docs/[page]" as="/docs/unity-actions"><a>Unity Actions</a></Link></Menu.Item>
           <Menu.Item key="6">option6</Menu.Item>
           <Menu.Item key="7">option7</Menu.Item>
           <Menu.Item key="8">option8</Menu.Item>
         </SubMenu>
-        <SubMenu key="deployment" icon={null} title="Deployment">
-          <Menu.Item key="9">Steam</Menu.Item>
+        <SubMenu key="gitlab" icon={<GitlabOutlined />} title="GitLab">
+          <Menu.Item key="9">option9</Menu.Item>
+        </SubMenu>
+        <SubMenu key="deployment" icon={<AppstoreAddOutlined />} title="Deployment">
+          <Menu.Item key="10">Steam</Menu.Item>
         </SubMenu>
       </Menu>
     </Sider>
