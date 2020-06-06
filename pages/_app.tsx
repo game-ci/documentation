d626add915a6ec0d04eb557ea8ed3d2c87d1a40e
Webber <webber@takken.io>
2020-06-06 15:50:59 +0200

    Markdown tables styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import 'antd/dist/antd.css';
 import '../styles/vars.css';
 import '../styles/global.css';
 import '../styles/layout.css';
+import '../styles/markdown.css';
 import 'highlight.js/styles/dracula.css';
 
 export default function App({ Component, pageProps }: AppProps) {
