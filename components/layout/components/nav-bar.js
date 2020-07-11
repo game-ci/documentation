d8f1e175a4c24a0b774025235a5151efad67d8d5
Webber <webber@takken.io>
2020-07-11 19:19:55 +0200

    Add references and backlinks

--- components/layout/components/nav-bar.js
+++ components/layout/components/nav-bar.js
@@ -1,20 +1,117 @@
 import { Layout, Menu } from 'antd';
 import Link from 'next/link';
+import { GithubOutlined, GitlabOutlined } from '@ant-design/icons';
+import UnityCiLogo from '../../../images/unity-ci-brand-logo-wordmark.svg';
+import DiscordLogo from '../../../images/discord-brand-logo-wordmark.svg';
 
 const { Header } = Layout;
-const { Item } = Menu;
+const { Item, ItemGroup, SubMenu } = Menu;
 
 const NavBar = () => (
   <Header className="header">
-    <div className="logo" />
-    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-      <Item key="2">Home</Item>
+    <a className="logo" href="/">
+      <UnityCiLogo height="30" />
+    </a>
+    <Menu
+      theme="dark"
+      mode="horizontal"
+      style={{ position: 'relative', display: 'flex', justifyContent: 'left' }}
+      defaultSelectedKeys={['1']}
+    >
+      <Item key="2">
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </Item>
       <Item key="1">
         <Link href="/docs">
           <a>Docs</a>
         </Link>
       </Item>
-      <Item key="3">nav 3</Item>
+    </Menu>
+    <Menu
+      theme="dark"
+      selectable={false}
+      mode="horizontal"
+      style={{ position: 'absolute', top: 0, right: 0 }}
+    >
+      <SubMenu icon={<GithubOutlined />} title="Source code">
+        <ItemGroup title="Project">
+          <Item icon={<GithubOutlined />} key="source:documentation">
+            <a target="_blank" rel="noreferrer" href="https://github.com/Unity-CI/Website">
+              Website
+            </a>
+          </Item>
+          <Item icon={<GitlabOutlined />} key="external:example:gitlab">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://gitlab.com/gableroux/unity3d-gitlab-ci-example"
+            >
+              Example CI project
+            </a>
+          </Item>
+        </ItemGroup>
+        <ItemGroup title="Docker">
+          <Item icon={<GitlabOutlined />} key="external:gableroux:unity3d">
+            <a target="_blank" rel="noreferrer" href="https://hub.docker.com/r/gableroux/unity3d">
+              GabLeRoux:Unity3D
+            </a>
+          </Item>
+        </ItemGroup>
+        <ItemGroup title="GitHub Actions">
+          <Item icon={<GithubOutlined />} key="external:action:builder">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/marketplace/actions/unity-builder"
+            >
+              Unity Builder
+            </a>
+          </Item>
+          <Item icon={<GithubOutlined />} key="external:action:test-runner">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/marketplace/actions/unity-test-runner"
+            >
+              Unity Test runner
+            </a>
+          </Item>
+          <Item icon={<GithubOutlined />} key="external:action:activate">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/marketplace/actions/unity-activate"
+            >
+              Unity Activate
+            </a>
+          </Item>
+          <Item icon={<GithubOutlined />} key="external:action:request-activation-file">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/marketplace/actions/unity-request-activation-file"
+            >
+              Unity Request Activation
+            </a>
+          </Item>
+          <Item icon={<GithubOutlined />} key="external:action:return-license">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/marketplace/actions/unity-return-license"
+            >
+              Unity Return License
+            </a>
+          </Item>
+        </ItemGroup>
+      </SubMenu>
+      <Item>
+        <Link href="/discord">
+          <DiscordLogo height={30} style={{ verticalAlign: '-0.75em' }} />
+        </Link>
+      </Item>
     </Menu>
   </Header>
 );
