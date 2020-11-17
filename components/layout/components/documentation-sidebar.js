d380aab082770a565f77001628e9192d43824687
Webber <webber@takken.io>
2020-11-17 01:07:44 +0100

    add custom icons

--- components/layout/components/documentation-sidebar.js
+++ components/layout/components/documentation-sidebar.js
@@ -1,14 +1,14 @@
 import { Menu, Layout } from 'antd';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { QuestionCircleOutlined } from '@ant-design/icons';
 import {
-  GithubOutlined,
-  CheckOutlined,
-  AppstoreAddOutlined,
-  GitlabOutlined,
-  QuestionCircleOutlined,
-  CiCircleOutlined,
-} from '@ant-design/icons';
+  SiTravisci,
+  SiGithubactions,
+  SiGitlab,
+  SiSteam,
+  AiOutlineInfoCircle,
+} from 'react-icons/all';
 
 const { Sider } = Layout;
 const { SubMenu, Item } = Menu;
@@ -18,7 +18,7 @@ const DocumentationSidebar = () => {
 
   const main = 'main';
   const current = currentKey.split('/')[2] || main;
-  const openSections = current === main ? [main] : [main, current];
+  const openSections = [current];
 
   return (
     <Sider className="site-layout-background" width={200} breakpoint="lg" collapsedWidth="0">
@@ -29,14 +29,14 @@ const DocumentationSidebar = () => {
         subMenuOpenDelay={1}
         style={{ height: '100%' }}
       >
-        <SubMenu key="main" icon={<CheckOutlined />} title="Unity CI">
+        <SubMenu key="main" icon={<AiOutlineInfoCircle />} title="Unity CI">
           <Item key="/docs">
             <Link href="/docs">
               <a>Introduction</a>
             </Link>
           </Item>
         </SubMenu>
-        <SubMenu key="github" icon={<GithubOutlined />} title="GitHub">
+        <SubMenu key="github" icon={<SiGithubactions />} title="GitHub">
           <Item key="/docs/github/getting-started">
             <Link href="/docs/[...documentation-page]" as="/docs/github/getting-started">
               <a>Getting started</a>
@@ -63,7 +63,7 @@ const DocumentationSidebar = () => {
             </Link>
           </Item>
         </SubMenu>
-        <SubMenu key="gitlab" icon={<GitlabOutlined />} title="GitLab">
+        <SubMenu key="gitlab" icon={<SiGitlab />} title="GitLab">
           <Item key="/docs/gitlab/getting-started">
             <Link href="/docs/[...documentation-page]" as="/docs/gitlab/getting-started">
               <a>Getting started</a>
@@ -100,14 +100,14 @@ const DocumentationSidebar = () => {
             </Link>
           </Item>
         </SubMenu>
-        <SubMenu key="travis" icon={<CiCircleOutlined />} title="Travis CI">
+        <SubMenu key="travis" icon={<SiTravisci />} title="Travis CI">
           <Item key="/docs/travis/getting-started">
             <Link href="/docs/[...documentation-page]" as="/docs/travis/getting-started">
               <a>Getting started</a>
             </Link>
           </Item>
         </SubMenu>
-        <SubMenu key="deployment" icon={<AppstoreAddOutlined />} title="Deployment">
+        <SubMenu key="deployment" icon={<SiSteam />} title="Deployment">
           <Item key="10" disabled>
             To be added
           </Item>
