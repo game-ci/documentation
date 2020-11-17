78d8da97a3bf5018b4566fed46c4a3112bfc1b5d
Webber Takken <webber@takken.io>
2020-11-17 01:10:55 +0100

    Add custom icons (#55)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { AppProps } from 'next/app';
+import { IconContext } from 'react-icons';
 
 import 'antd/dist/antd.css';
 // import 'antd/dist/antd.dark.css';
@@ -10,5 +11,9 @@ import '../styles/markdown.css';
 import 'highlight.js/styles/dracula.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <IconContext.Provider value={{ className: 'anticon' }}>
+      <Component {...pageProps} />
+    </IconContext.Provider>
+  );
 }
