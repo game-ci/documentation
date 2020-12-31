4d36f4c14dbcd9e8fd89666b0b19cbd3ea8cd05f
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2020-12-31 11:35:30 -0500

    Add favicon for new game.ci logo
    Used https://realfavicongenerator.net/ to generated them

--- pages/docs/index.js
+++ pages/docs/index.js
@@ -9,7 +9,15 @@ export default function Page() {
     <DocumentationLayout>
       <Head>
         <title>GameCI</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="manifest" href="/icons/site.webmanifest" />
+        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#373445" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
 
       <main className="ant-typography">
