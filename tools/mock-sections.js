ec058fc87c5eaa10b3df6cd97c9274aeb4bd3f8b
Webber <webber@takken.io>
2021-01-25 23:55:47 +0100

    Add mock sections

---
+++ tools/mock-sections.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    title: 'What is activating?',
+    summary: 'Activation is something nice...',
+    body:
+      'Activation is something nice this lorem ipsum dolor sit amet, consectetur adepsicing elit yadda yadda etc.',
+    categories: ['Activation', 'Introduction'],
+  },
+  {
+    title: 'How to activate?',
+    summary: 'Activation goes like this...',
+    body:
+      'Activation goes like this lorem ipsum dolor sit amet, consectetur adepsicing elit yadda yadda etc.',
+    categories: ['Activation', 'GitHub Actions'],
+  },
+  {
+    title: 'Why should I activate?',
+    summary: 'Activating is necessary because...',
+    body:
+      'Activating is necessary because lorem ipsum dolor sit amet, consectetur adepsicing elit yadda yadda etc.',
+    categories: ['GitHub Actions'],
+  },
+];
