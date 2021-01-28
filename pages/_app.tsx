2129acc86e736eaaccdd538ea01a6f13ac812f0d
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2021-01-28 11:11:23 -0500

    WIP: some attempts to get less working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,20 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { AppProps } from 'next/app';
-import { IconContext } from 'react-icons';
+// import { IconContext } from 'react-icons';
 
 // import 'antd/dist/antd.dark.css';
-import '../styles/vars.css';
-import '../styles/global.css';
-import '../styles/layout.css';
-import '../styles/markdown.css';
-import 'highlight.js/styles/dracula.css';
+// import 'antd/dist/antd.less';
+// import '../styles/vars.css';
+// import '../styles/global.css';
+// import '../styles/layout.css';
+// import '../styles/markdown.css';
+// import 'highlight.js/styles/dracula.css';
 
-import '../styles/home/header.less';
-import '../styles/home/home.less';
-import '../styles/home/footer.less';
-import '../styles/home/responsive.less';
+// import '../styles/home/header.less';
+// import '../styles/home/home.less';
+// import '../styles/home/footer.less';
+// import '../styles/home/responsive.less';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <IconContext.Provider value={{ className: 'anticon' }}>
-      <Component {...pageProps} />
-    </IconContext.Provider>
-  );
+  return <Component {...pageProps} />;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,14 @@
-import Link from 'next/link';
-import NavBar from '@/components/layout/components/nav-bar';
-import { Button } from 'antd';
 import React from 'react';
 
-import Banner from '@/components/home/banner';
-import Section1 from '@/components/home/section1';
-import Section2 from '@/components/home/section2';
-import Section3 from '@/components/home/section3';
-import FooterNavigation from '@/components/home/footerNavigation';
-import GameCiLogo from '../images/game-ci-brand-logo-wordmark.svg';
+import Home from '@/components/home';
+
+import BaseLayout from '@/components/layout/base-layout';
 
 const Index = () => {
   return (
-    <>
-      <NavBar />
-
-      <p align="center">
-        <GameCiLogo height="500" width="500" />
-      </p>
-      <p align="center">
-        <Button>
-          <Link href="/docs/github/getting-started">I use Github</Link>
-        </Button>
-        <Button>
-          <Link href="/docs/gitlab/getting-started">I use Gitlab</Link>
-        </Button>
-      </p>
-
-      <Banner isMobile={false} />
-      <Section1 isMobile={false} />
-      <Section2 isMobile={false} />
-      <Section3 />
-      <FooterNavigation />
-    </>
+    <BaseLayout>
+      <Home />
+    </BaseLayout>
   );
 };
 
