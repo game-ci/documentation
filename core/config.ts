8ed01e54c2be463ee9b47c40b6cd7d1c127499b4
Webber <webber@takken.io>
2021-01-29 22:07:27 +0100

    Create search configuration

---
+++ core/config.ts
@@ -0,0 +1,17 @@
+export default {
+  search: {
+    applicationId: 'W4X62KM9QE',
+    index: 'documentation_sections',
+    publicApiKey: '4a06011ed8a467811866c27ca4c1a367',
+    settings: {
+      // Select the attributes you want to search in
+      searchableAttributes: ['title', 'summary', 'body', 'categories'],
+      // Define business metrics for ranking and sorting
+      customRanking: ['desc(record_index)'],
+      // Set up some attributes to filter results on
+      attributesForFaceting: ['categories'],
+      // Define the attribute we want to distinct on
+      attributeForDistinct: 'ObjectID',
+    },
+  },
+};
--- core/search/index.ts
+++ core/search/index.ts
@@ -1,20 +1,8 @@
 import algoliasearch from 'algoliasearch';
+import config from '@/core/config';
 
-const searchOnlyApiKey = '4a06011ed8a467811866c27ca4c1a367';
+const client = algoliasearch(config.search.applicationId, config.search.publicApiKey);
 
-const client = algoliasearch('W4X62KM9QE', searchOnlyApiKey);
-
-const index = client.initIndex('demo_media');
-
-index.setSettings({
-  // Select the attributes you want to search in
-  searchableAttributes: ['post_title', 'author_name', 'categories', 'content'],
-  // Define business metrics for ranking and sorting
-  customRanking: ['desc(post_date)', 'desc(record_index)'],
-  // Set up some attributes to filter results on
-  attributesForFaceting: ['categories'],
-  // Define the attribute we want to distinct on
-  attributeForDistinct: 'post_id',
-});
+const index = client.initIndex(config.search.index);
 
 export { client, index };
