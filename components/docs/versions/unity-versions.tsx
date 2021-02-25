6a78d66b03c11c987be13174569996a272ba2632
Webber <webber@takken.io>
2021-02-26 00:47:06 +0100

    Fix rendering stack

--- components/docs/versions/unity-versions.tsx
+++ components/docs/versions/unity-versions.tsx
@@ -1,6 +1,6 @@
 import UnityVersion from '@/components/docs/versions/unity-version';
 import { Collapse } from 'antd';
-import React, { Dispatch, SetStateAction } from 'react';
+import React, { Dispatch, SetStateAction, useEffect } from 'react';
 import { useFirestore, useFirestoreCollectionData } from 'reactfire';
 
 interface Props {
@@ -11,6 +11,7 @@ interface Props {
 const UnityVersions = ({ selectedRepoVersion, setIsLoading }: Props) => {
   if (!selectedRepoVersion) return null;
 
+  useEffect(() => {});
   const ciJobs = useFirestore()
     .collection('ciJobs')
     .orderBy('editorVersionInfo.major', 'desc')
@@ -20,12 +21,16 @@ const UnityVersions = ({ selectedRepoVersion, setIsLoading }: Props) => {
 
   const { status, data } = useFirestoreCollectionData<{ [key: string]: any }>(ciJobs);
   const isLoading = status === 'loading';
-  setIsLoading(isLoading);
+  useEffect(() => {
+    setIsLoading(isLoading);
+  });
 
   const loading = <p>Fetching versions...</p>;
   return (
     <Collapse>
-      {isLoading ? loading : data.map((version) => <UnityVersion data={version} />)}
+      {isLoading
+        ? loading
+        : data.map((version) => <UnityVersion key={version.NO_ID_FIELD} data={version} />)}
     </Collapse>
   );
 };
