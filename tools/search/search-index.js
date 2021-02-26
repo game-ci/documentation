b5b6294bbfd303d91b4432fe51ad86eec2cca1dc
Webber <webber@takken.io>
2021-02-26 20:55:29 +0100

    Possible fix for pipeline failure

--- tools/search/search-index.js
+++ tools/search/search-index.js
@@ -5,11 +5,12 @@ export class SearchIndex {
   static name = `search`;
 
   static getForCurrentEnvironment() {
+    const prefix = `${this.name}_`;
     const index = currentBranch()
       .replace(/\//g, '-') // convert slash (/) to hyphen (-)
       .replace(/[^\w-]/g, '') // remove non-word-characters except hyphen (-)
       .replace(/-+/g, '-') // deduplicate hyphens
-      .replace(/^/, `${this.name}_`); // prefix with index name
+      .replace(/^/, prefix); // prefix with index name
 
     console.log(`info  - Using search index "${index}".`);
 
