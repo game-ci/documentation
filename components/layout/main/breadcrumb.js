2b0dae8f910172f30ed49f1951638484151dfb32
Webber <webber@takken.io>
2021-03-04 01:19:05 +0100

    Fix broken links

--- components/layout/main/breadcrumb.js
+++ components/layout/main/breadcrumb.js
@@ -1,21 +1,32 @@
+import { MenuNode } from '@/tools/menu/menu-node';
 import { MenuSegment } from '@/tools/menu/menu-segment';
+import { MenuStructure } from '@/tools/menu/menu-structure';
 import Link from 'next/link';
 import { Breadcrumb } from 'antd';
 import { HomeOutlined } from '@ant-design/icons';
 import usePathSegments from '@/core/routing/use-path-segments';
+import { useContext } from 'react';
+import MenuContext from '../docs/menu/menu-context';
 
 const { Item } = Breadcrumb;
 
 const BreadcrumbWrapper = () => {
   const segments = usePathSegments(<HomeOutlined />);
+  const { menuStructure } = useContext(MenuContext);
+
   return (
     <Breadcrumb>
       {segments.map(({ url, name }) => {
         if (MenuSegment.isVersion(name)) return null;
 
+        // If segment matches folder in generated menu-structure, determine its first page
+        const menuNode = MenuStructure.getNodeByUrl(menuStructure, url);
+        const nearestUrl = MenuNode.findFirstPage(menuNode);
+        const href = nearestUrl ? `/docs/${nearestUrl}` : url;
+
         return (
-          <Item key={url} style={{ textTransform: 'capitalize' }}>
-            <Link href={url}>
+          <Item key={href} style={{ textTransform: 'capitalize' }}>
+            <Link href={href}>
               <a>{name}</a>
             </Link>
           </Item>
--- tools/menu/menu-node.ts
+++ tools/menu/menu-node.ts
@@ -13,15 +13,72 @@ export class MenuNode {
     return get(node, 'meta.type') === 'file';
   }
 
+  static isFolder(node: MenuNode) {
+    return get(node, 'meta.type') === 'folder';
+  }
+
   static isPage(node: MenuNode) {
     return this.isFile(node);
   }
 
-  static hasChildren(node: any) {
+  static isVersion(node: MenuNode) {
+    return get(node, 'meta.type') === 'version';
+  }
+
+  static hasChildren(node: MenuNode) {
     return typeof node === 'object';
   }
 
   static hasVersionContainer(node: MenuNode) {
     return has(node, MenuSegment.versionContainerIdentifier);
+  }
+
+  static getCurrentVersion(versionContainerNode: MenuNode) {
+    return get(versionContainerNode, 'meta.currentVersion');
+  }
+
+  static findFirstPage(menuNode: MenuNode) {
+    if (menuNode && MenuNode.isFolder(menuNode)) {
+      const page = MenuNode.findFirstPageInFolder(menuNode);
+      return page?.meta?.path || null;
+    }
+
+    return null;
+  }
+
+  static findFirstPageInFolder(menuNode: MenuNode) {
+    const fileMetas = [];
+    const containers = [];
+
+    for (const [segment, node] of Object.entries(menuNode)) {
+      if (MenuSegment.isMeta(segment)) continue;
+
+      if (MenuNode.isFile(node)) {
+        fileMetas.push(node);
+      }
+
+      if (MenuSegment.isVersionContainer(segment)) {
+        const currentVersion = this.getCurrentVersion(node);
+        containers.push(node[currentVersion]);
+      }
+
+      if (MenuNode.isFolder(node) || MenuNode.isVersion(node)) {
+        containers.push(node);
+      }
+    }
+
+    // Todo sort by fileMeta.meta.order asc
+    if (fileMetas.length > 0) {
+      return fileMetas[0];
+    }
+
+    for (const container of containers) {
+      const result = this.findFirstPageInFolder(container);
+      if (result) {
+        return result;
+      }
+    }
+
+    return null;
   }
 }
