8582070b8501942196321efa51ee7251be22ff98
Raphaël Côté <raphael.cote@ellipse-synergie.com>
2021-06-17 12:00:07 -0400

    Add a favicon in the landing page + refactor headers (#186)

---
+++ components/pages/home/header.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react';
+import Head from 'next/head';
+
+const Header = () => (
+  <Head>
+    <title>GameCI</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+    <link rel="manifest" href="/icons/site.webmanifest" />
+    <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
+    <link rel="shortcut icon" href="/icons/favicon.ico" />
+    <meta name="msapplication-TileColor" content="#373445" />
+    <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+    <meta name="theme-color" content="#ffffff" />
+  </Head>
+);
+
+export default Header;
