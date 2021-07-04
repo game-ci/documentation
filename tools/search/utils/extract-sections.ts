9a78e5cb339b0d2b0be0f5ee72fdf96253ea714a
Webber <webber@takken.io>
2021-07-05 00:19:47 +0200

    Use parsable markdown and descriptive errors

--- tools/search/utils/extract-sections.ts
+++ tools/search/utils/extract-sections.ts
@@ -17,6 +17,15 @@ const extractTitle = (line) => {
     console.log('\u001B[33m%s\u001B[0m  - %s', 'warn', 'Unable to extract title from', line);
     return '';
   }
+
+  if (typeof line !== 'string') {
+    // eslint-disable-next-line no-console
+    console.error(
+      `Expected heading to consist of a string. That way we can easily use them as anchors.
+      The following does not adhere to this rule: "${JSON.stringify(line, null, 2)}"`,
+    );
+  }
+
   return line.replace(/^[\s#]*/, '').trim();
 };
 
@@ -102,6 +111,7 @@ export default function extractSections(meta, contents) {
   }
 
   if (skippedLines >= 1) {
+    // eslint-disable-next-line no-console
     console.log(`Skipped ${skippedLines} lines that occurred before any title`);
   }
 
