7f3638e7894f512fa297af6332df600a69f1ca76
Waryserpant122 <yashshah123boy@gmail.com>
2021-08-09 02:19:55 +0530

    Fixed the bug of version sorted using custom sort function

--- components/docs/versions/unity-versions.tsx
+++ components/docs/versions/unity-versions.tsx
@@ -28,6 +28,41 @@ const UnityVersions = ({ selectedRepoVersion, setIsLoading }: Props) => {
   const loading = <p>Fetching versions...</p>;
   const failures = isLoading ? [] : data.filter((version) => version.status === 'failed');
 
+  if (data) {
+    // Sorting the data based on the version numbers to maintain the version order
+    data.sort((a, b) => {
+      const numberStringA = a.NO_ID_FIELD.split('-')[1];
+      const numberStringB = b.NO_ID_FIELD.split('-')[1];
+
+      let numberArrayA = numberStringA.split('.');
+      let numberArrayB = numberStringB.split('.');
+
+      if (numberArrayA[0] > numberArrayB[0]) return -1;
+      if (numberArrayA[0] < numberArrayB[0]) return 1;
+
+      // Assuming here f keyword is present in last version.
+      const temporaryA = numberArrayA[2].split('f');
+      const temporaryB = numberArrayB[2].split('f');
+
+      numberArrayA.pop();
+      numberArrayA = [...numberArrayA, ...temporaryA];
+      numberArrayB.pop();
+      numberArrayB = [...numberArrayB, ...temporaryB];
+
+      // sorting based on the versions. Using Multiplication to give priority
+      const productionA =
+        Number.parseInt(numberArrayA[1], 10) * 1000000 +
+        Number.parseInt(numberArrayA[2], 10) * 1000 +
+        Number.parseInt(numberArrayA[3], 10);
+      const productionB =
+        Number.parseInt(numberArrayB[1], 10) * 1000000 +
+        Number.parseInt(numberArrayB[2], 10) * 1000 +
+        Number.parseInt(numberArrayB[3], 10);
+
+      return productionB - productionA;
+    });
+  }
+
   return (
     <main style={{ paddingTop: 24 }}>
       {failures.length > 0 && (
