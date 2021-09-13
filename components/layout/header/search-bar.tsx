ee7f5085fa817e4ff4b5852f650e6a60b6f0ff52
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2021-09-13 00:28:11 -0400

    Remove previous search implementation's config

--- components/layout/header/search-bar.tsx
+++ components/layout/header/search-bar.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { DocSearch } from '@docsearch/react';
 import '@docsearch/css';
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import 'firebase/auth';
 import 'firebase/firestore';
 import { AppProps } from 'next/app';
-import { IconContext } from 'react-icons';
-import { InstantSearch } from 'react-instantsearch-dom';
-import { client as searchClient, indexName as searchIndex } from '@/core/search';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { reducer } from 'logic';
@@ -20,11 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <FirebaseAppProvider firebaseConfig={config.firebase}>
-        <InstantSearch searchClient={searchClient} indexName={searchIndex}>
-          <IconContext.Provider value={{ className: 'anticon' }}>
-            <Component {...pageProps} />
-          </IconContext.Provider>
-        </InstantSearch>
+        <Component {...pageProps} />
       </FirebaseAppProvider>
     </Provider>
   );
