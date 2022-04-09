566b0ea8345f63ce08550273a5e4dfa030062c84
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2022-04-09 14:25:48 -0400

    Revert "Remove search config"
    This reverts commit ac39e2bf9bf14d92b21c60d0f3b5db7f507c78f4.

--- core/config.ts
+++ core/config.ts
@@ -1,3 +1,5 @@
+import { getIsomorphicValue } from '@/tools/utils/get-isomorphic-value';
+
 export default {
   baseUrl: 'https://game.ci',
   docsPath: '/docs',
@@ -12,4 +14,20 @@ export default {
     measurementId: 'G-1BGVS7HDW8',
   },
   backendUrl: 'https://europe-west3-unity-ci-versions.cloudfunctions.net',
+  search: {
+    applicationId: 'W4X62KM9QE',
+    index: getIsomorphicValue('SEARCH_INDEX'),
+    publicApiKey: '4a06011ed8a467811866c27ca4c1a367',
+    hitsPerPage: 12,
+    settings: {
+      // Select the attributes you want to search in
+      searchableAttributes: ['title', 'summary', 'body', 'version', 'categories', 'filename'],
+      // Define business metrics for ranking and sorting
+      customRanking: ['asc(level)', 'desc(title)'],
+      // Set up some attributes to filter results on
+      attributesForFaceting: ['filename'],
+      // Define the attribute we want to distinct on
+      attributeForDistinct: 'ObjectID',
+    },
+  },
 };
