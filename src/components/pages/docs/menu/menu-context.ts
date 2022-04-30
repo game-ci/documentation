0de429609258df62ec112ec8f2ce405d83cc8a34
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2022-04-30 18:10:50 -0400

    Get rid of unused components

--- src/components/pages/docs/menu/menu-context.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-import menuStructure from '@site/src/core/menu-structure';
-
-const defaultContext = { menuStructure };
-
-const MenuContext = createContext(defaultContext);
-MenuContext.displayName = 'MenuContext';
-
-export default MenuContext;
