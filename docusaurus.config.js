614bc60518343a96b4cf4a20cc8d52bc0ecf5d33
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2022-05-04 19:39:25 -0400

    Add tailwindcss support to docusaurus
    Followed https://dev.to/sajclarke_62/using-tailwindcss-v3-in-docusaurus-in-5-steps-5c26

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,6 +16,20 @@ const config = {
   organizationName: 'game-ci', // Usually your GitHub org/user name.
   projectName: 'documentation', // Usually your repo name.
 
+  plugins: [
+    async function tailwindcss(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    },
+  ],
+
   presets: [
     [
       'classic',
