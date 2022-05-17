95ad0db874b7a08dec12e8ecbd0f45919c621812
Webber <webber@takken.io>
2022-05-17 23:17:36 +0200

    bring back typescript react file types

---
+++ src/@types/filetypes.d.ts
@@ -0,0 +1,9 @@
+declare module '*.module.scss' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.scss' {
+  const source: string;
+  export default source;
+}
