75484db4be261d0ef664b71d3c38d3b0249a1253
Webber <webber@takken.io>
2022-05-18 00:25:39 +0200

    swizzle Root component for global state and auth

---
+++ src/theme/Root.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import 'firebase/auth';
+import 'firebase/firestore';
+import { IconContext } from 'react-icons';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { FirebaseAppProvider } from 'reactfire';
+import config from '@site/src/core/config';
+import { reducer } from '@site/src/logic';
+
+const store = configureStore({ reducer });
+
+interface Props {
+  children: React.ReactNode;
+}
+
+function Root({ children }: Props): JSX.Element {
+  return (
+    <Provider store={store}>
+      <FirebaseAppProvider firebaseConfig={config.firebase}>
+        {/* Todo - Remove anticon className when removing AntDesign */}
+        <IconContext.Provider value={{ className: 'anticon' }}>{children}</IconContext.Provider>
+      </FirebaseAppProvider>
+    </Provider>
+  );
+}
+
+export default Root;
