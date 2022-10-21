78e73c45327360f835dfcef06ef9e579b17b2794
Webber <webber@takken.io>
2022-10-21 21:02:21 +0200

    chore: add circle-ci sponsor

--- src/components/pages/home/section/getting-started.tsx
+++ src/components/pages/home/section/getting-started.tsx
@@ -1,37 +1,32 @@
 import FadeIntoView from '@site/src/components/molecules/animations/fade-into-view';
 import Link from '@docusaurus/Link';
 import React from 'react';
+import styles from '@site/src/components/pages/home/section/section.module.scss';
+import cx from 'classnames';
+import Section from '@site/src/components/pages/home/section/section';
 
 const GettingStarted = () => {
-  const buttonClasses =
-    'py-4 px-6 mx-3 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg';
-
   return (
-    <div className="bg-white dark:bg-gray-800 ">
-      <div className="text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
-        <FadeIntoView>
-          <h2 className="text-3xl font-extrabold text-black dark:text-white sm:text-4xl">
-            <span className="block">Quickly get started</span>
-            <span>My automation runs on</span>
-          </h2>
-        </FadeIntoView>
-        <div className="lg:mt-0 lg:flex-shrink-0">
-          <FadeIntoView>
-            <div className="mt-12 inline-flex">
-              <Link to="/docs/github/getting-started" className={buttonClasses}>
-                github
-              </Link>
-              <Link to="/docs/gitlab/getting-started" className={buttonClasses}>
-                gitlab
-              </Link>
-              <Link to="/docs/circleci/getting-started" className={buttonClasses}>
-                circleci
-              </Link>
-            </div>
-          </FadeIntoView>
+    <Section className={styles.gettingStartedSection}>
+      <FadeIntoView>
+        <h2 className={cx('text-center text-5xl font-bold mb-10', styles.title)}>Get started</h2>
+      </FadeIntoView>
+
+      <FadeIntoView>
+        <p className="text-center text-xl">My automation runs on</p>
+        <div className="mt-6 inline-flex gap-2">
+          <Link to="/docs/github/getting-started" className={styles.button}>
+            GitHub
+          </Link>
+          <Link to="/docs/gitlab/getting-started" className={styles.button}>
+            GitLab
+          </Link>
+          <Link to="/docs/circleci/getting-started" className={styles.button}>
+            CircleCI
+          </Link>
         </div>
-      </div>
-    </div>
+      </FadeIntoView>
+    </Section>
   );
 };
 
