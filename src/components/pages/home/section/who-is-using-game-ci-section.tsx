ef29646747c4135215e12efc26c291b9ca5ea6af
EliasVal <46247153+EliasVal@users.noreply.github.com>
2023-08-25 22:48:19 +0300

    Devs that chose us section - Moved GH, GL, CCI SVGs to /static/assets/images/icons - 'Finished' Devs that chose us section (more logos needed) - Reintroduced '.vscode' directory from .gitignore to support @apply rule

--- src/components/pages/home/section/who-is-using-game-ci-section.tsx
+++ src/components/pages/home/section/who-is-using-game-ci-section.tsx
@@ -5,8 +5,60 @@ import styles from './section.module.scss';
 const WhoIsUsingGameCiSection = () => {
   return (
     <Section className={styles.whoIsUsingGameCiSection}>
-      <h2>Developers that chose us</h2>
-      {/* Todo: 5 columns 3 rows for mobile - 10 columns 4 rows for large screens */}
+      <h1>Developers that chose us</h1>
+      <div className={styles.devsGrid}>
+        <div className={styles.gridItem}>
+          <a href="https://www.madronagames.com/">
+            <img src="/assets/images/devs/madronagames-logo.png" alt="Madrona Games" />
+          </a>
+        </div>
+        <div className={styles.gridItem}>
+          <a href="https://totemastudio.com/">
+            <img src="/assets/images/devs/totemastudio-logo.svg" alt="Totema Studios" />
+          </a>
+        </div>
+        <div className={styles.gridItem}>
+          <a href="/">
+            <img src="/assets/images/devs/" alt="" />
+          </a>
+        </div>
+        <div className={styles.gridItem}>
+          <a href="/">
+            <img src="/assets/images/devs/" alt="" />
+          </a>
+        </div>
+        <div className={styles.gridItem}>
+          <a href="/">
+            <img src="/assets/images/devs/" alt="" />
+          </a>
+        </div>
+        <div className={styles.gridItem}>
+          <a href="/">
+            <img src="/assets/images/devs/" alt="" />
+          </a>
+        </div>
+        <div className={styles.gridItem}>
+          <a href="/">
+            <img src="/assets/images/devs/" alt="" />
+          </a>
+        </div>
+        <div className={styles.gridItem}>
+          <a href="/">
+            <img src="/assets/images/devs/" alt="" />
+          </a>
+        </div>
+        <div className={styles.gridItem}>
+          <a href="/">
+            <img src="/assets/images/devs/" alt="" />
+          </a>
+        </div>
+        <div className={styles.gridItem}>
+          <a href="/">
+            <img src="/assets/images/devs/" alt="" />
+          </a>
+        </div>
+      </div>
+      <h4>And more than 3500+ other developers</h4>
     </Section>
   );
 };
