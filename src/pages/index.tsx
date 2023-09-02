9c13a8bf335b9f743509dd3dd22f48d0d9e89001
EliasVal <46247153+EliasVal@users.noreply.github.com>
2023-09-02 19:52:43 +0300

    disabled unused sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,26 +3,26 @@ import Layout from '@theme/Layout';
 import ThePerksSection from '@site/src/components/pages/home/section/the-perks-section';
 import OpeningSection from '@site/src/components/pages/home/section/opening-section';
 import SponsorsSection from '@site/src/components/pages/home/section/sponsors-section';
-import VisualRepresentationOfWhatWeDoSection from '@site/src/components/pages/home/section/visual-representation-of-what-we-do';
-import OurVisionSection from '@site/src/components/pages/home/section/our-vision-section';
+// import VisualRepresentationOfWhatWeDoSection from '@site/src/components/pages/home/section/visual-representation-of-what-we-do';
+// import OurVisionSection from '@site/src/components/pages/home/section/our-vision-section';
 import PricingSection from '@site/src/components/pages/home/section/pricing-section';
-import CodeExamplesSection from '@site/src/components/pages/home/section/code-examples-section';
+// import CodeExamplesSection from '@site/src/components/pages/home/section/code-examples-section';
 import WhoIsUsingGameCiSection from '@site/src/components/pages/home/section/who-is-using-game-ci-section';
-import GallerySection from '@site/src/components/pages/home/section/gallery-section';
+// import GallerySection from '@site/src/components/pages/home/section/gallery-section';
 
 export default function Home(): React.JSX.Element {
   return (
     <Layout>
       <OpeningSection />
       {/* <GettingStarted /> */}
-      <OurVisionSection />
-      <VisualRepresentationOfWhatWeDoSection />
+      {/* <OurVisionSection /> */}
+      {/* <VisualRepresentationOfWhatWeDoSection /> */}
       <ThePerksSection />
       <SponsorsSection />
       <WhoIsUsingGameCiSection />
-      <CodeExamplesSection />
+      {/* <CodeExamplesSection /> */}
       <PricingSection />
-      <GallerySection />
+      {/* <GallerySection /> */}
     </Layout>
   );
 }
