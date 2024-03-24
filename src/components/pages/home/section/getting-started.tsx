d0cdba788c899d8cbbb98eef6bf327b1b745a52a
Kathy Lee <kathy.wingkilee@gmail.com>
2024-03-24 20:51:07 +0800

    Fix logo issue in light mode and update HTML tag structure

--- src/components/pages/home/section/getting-started.tsx
+++ src/components/pages/home/section/getting-started.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import cx from 'classnames';
 import styles from '@site/src/components/pages/home/section/section.module.scss';
 import FadeIntoView from '@site/src/components/molecules/animations/fade-into-view';
+import GitHubLogo from '@site/static/assets/images/icons/github.svg';
+import GitLabLogo from '@site/static/assets/images/icons/gitlab.svg';
+import CircleCiLogo from '@site/static/assets/images/icons/circleci.svg';
 
 const GettingStarted = () => {
   return (
@@ -14,19 +17,13 @@ const GettingStarted = () => {
       <FadeIntoView className={styles.gettingStartedLinks}>
         <div className="w-full mt-6 inline-flex gap-10 flex-col md:flex-row items-center justify-center pb-16">
           <Link to="/docs/github/getting-started" className={styles.button}>
-            <span>
-              <img src="/assets/images/icons/github.svg" alt="" />
-            </span>
+            <GitHubLogo />
           </Link>
           <Link to="/docs/gitlab/getting-started" className={styles.button}>
-            <span>
-              <img src="/assets/images/icons/gitlab.svg" alt="" />
-            </span>
+            <GitLabLogo />
           </Link>
           <Link to="/docs/circleci/getting-started" className={styles.button}>
-            <span>
-              <img src="/assets/images/icons/circleci.svg" alt="" />
-            </span>
+            <CircleCiLogo />
           </Link>
         </div>
       </FadeIntoView>
--- src/components/pages/home/section/sponsors-section.tsx
+++ src/components/pages/home/section/sponsors-section.tsx
@@ -4,6 +4,7 @@ import cx from 'classnames';
 import FadeIntoView from '@site/src/components/molecules/animations/fade-into-view';
 import styles from '@site/src/components/pages/home/section/section.module.scss';
 import Section from '@site/src/components/pages/home/section/section';
+import CircleCiLogo from '@site/static/assets/images/icons/circleci.svg';
 
 const SponsorsSection = () => {
   return (
@@ -13,13 +14,16 @@ const SponsorsSection = () => {
       </FadeIntoView>
 
       <FadeIntoView>
-        <div className="mt-12 inline-flex">
-          <Link to="https://circleci.com?utm_source=game-ci" className="text-black dark:text-white">
-            <span className={styles.linkSpan}>
-              <img width={120} src="/assets/images/icons/circleci.svg" alt="CircleCI" />
-            </span>
-          </Link>
-        </div>
+        <ul className="mt-12 flex gap-4 list-none p-0">
+          <li>
+            <Link
+              to="https://circleci.com?utm_source=game-ci"
+              className="text-black dark:text-white"
+            >
+              <CircleCiLogo className={styles.logo} />
+            </Link>
+          </li>
+        </ul>
       </FadeIntoView>
     </Section>
   );
