8be679890465a809820e5a99656f41964f6001ce
Kathy Lee <kathy.wingkilee@gmail.com>
2024-04-06 11:37:53 +0800

    Update header section and serve background images locally

--- src/components/pages/about-us/section/header-section/header-section.tsx
+++ src/components/pages/about-us/section/header-section/header-section.tsx
@@ -1,33 +1,28 @@
 import React from 'react';
+import desktopBackground from './pictures/background-desktop.webp';
+import tabletBackground from './pictures/background-tablet.webp';
+import mobileBackground from './pictures/background-mobile.webp';
 
 const HeaderSection = (): React.JSX.Element => {
   return (
-    <div className="2xl:container 2xl:mx-auto py-4 sm:py-6 lg:px-20 md:pt-8 md:px-6 px-4">
+    <section className="2xl:container 2xl:mx-auto py-4 sm:py-6 lg:px-20 md:pt-8 md:px-6 px-4">
       <div className="relative">
         <div className="absolute top-0 left-0 w-full h-full">
           <div className="w-full h-full flex flex-col justify-center items-center bg-indigo-700 bg-opacity-20 text-white sm:px-12 px-6 rounded-md">
-            <h2 className="font-bold lg:text-4xl text-3xl lg:leading-9 leading-7">About us</h2>
+            <h1 className="font-bold lg:text-4xl text-3xl lg:leading-9 leading-7">About us</h1>
           </div>
         </div>
         <div>
-          <img
-            className="rounded-md lg:block hidden w-full"
-            src="https://i.ibb.co/17q26WL/qw.png"
-            alt="A person Coding"
-          />
+          <img className="rounded-md lg:block hidden w-full" src={desktopBackground} alt="" />
           <img
             className="rounded-md lg:hidden sm:block hidden w-full"
-            src="https://i.ibb.co/pZX8cKM/Rectangle-122-1.png"
-            alt="A person Coding"
+            src={tabletBackground}
+            alt=""
           />
-          <img
-            className="rounded-md sm:hidden block w-full"
-            src="https://i.ibb.co/b3YLqxC/Rectangle-122.png"
-            alt="A person Coding"
-          />
+          <img className="rounded-md sm:hidden block w-full" src={mobileBackground} alt="" />
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
