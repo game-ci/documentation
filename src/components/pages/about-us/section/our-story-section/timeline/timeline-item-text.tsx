8a4c4458c53315efa6c409a575e22159901986b9
Kathy Lee <kathy.wingkilee@gmail.com>
2024-04-06 15:53:30 +0800

    Remove opacity for better contrast

--- src/components/pages/about-us/section/our-story-section/timeline/timeline-item-text.tsx
+++ src/components/pages/about-us/section/our-story-section/timeline/timeline-item-text.tsx
@@ -9,7 +9,7 @@ const TimelineItemText = ({ title, subTitle }: Props): React.JSX.Element => {
   return (
     <div className="w-full">
       <h3 className="font-semibold text-xl leading-5">{title}</h3>
-      <p className="font-normal text-base leading-6 mt-2 opacity-75">{subTitle}</p>
+      <p className="font-normal text-base leading-6 mt-2">{subTitle}</p>
     </div>
   );
 };
