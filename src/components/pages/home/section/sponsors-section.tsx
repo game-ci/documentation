50c83df81895df9328804c9d9e5074d3c67b0dfa
Webber <webber@takken.io>
2024-09-19 00:40:59 +0200

    feat: update contributions section, including open collective

--- src/components/pages/home/section/sponsors-section.tsx
+++ src/components/pages/home/section/sponsors-section.tsx
@@ -1,23 +1,19 @@
-import Link from '@docusaurus/Link';
 import React from 'react';
 import FadeIntoView from '@site/src/components/molecules/animations/fade-into-view';
 import Section from '@site/src/components/pages/home/section/section';
-import CircleCiLogo from '@site/static/assets/images/icons/circleci.svg';
-import { getUrlWithUtmParameters } from '@site/src/core/url';
 
 const SponsorsSection = () => {
-  const circleCiUrl = getUrlWithUtmParameters('https://circleci.com');
-
   return (
-    <Section title="Sponsors">
+    <Section title="Sponsors" className="bg-white dark:bg-[#020612]">
       <FadeIntoView>
-        <ul className="mt-12 flex gap-4 list-none p-0">
-          <li>
-            <Link to={circleCiUrl}>
-              <CircleCiLogo className="w-[120px] fill-brand-text-light dark:fill-brand-text-dark" />
-            </Link>
-          </li>
-        </ul>
+        <iframe
+          title="Game CI Sponsors"
+          height="667"
+          width="800"
+          src="https://opencollective.com/game-ci/banner.html"
+          frameBorder="0"
+          scrolling="no"
+        />
       </FadeIntoView>
     </Section>
   );
