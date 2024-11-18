958da387987fe93f09af53cb1b6f5e271c671462
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2024-11-17 20:14:54 -0500

    Also comment the import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import OpeningSection from '@site/src/components/pages/home/section/opening-sect
 import SponsorsSection from '@site/src/components/pages/home/section/sponsors-section';
 // import VisualRepresentationOfWhatWeDoSection from '@site/src/components/pages/home/section/visual-representation-of-what-we-do';
 // import OurVisionSection from '@site/src/components/pages/home/section/our-vision-section';
-import PricingSection from '@site/src/components/pages/home/section/pricing-section';
+// import PricingSection from '@site/src/components/pages/home/section/pricing-section';
 // import CodeExamplesSection from '@site/src/components/pages/home/section/code-examples-section';
 import WhoIsUsingGameCiSection from '@site/src/components/pages/home/section/who-is-using-game-ci-section';
 // import GallerySection from '@site/src/components/pages/home/section/gallery-section';
